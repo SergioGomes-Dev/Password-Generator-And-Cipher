49bcc5c27c23bfc9545c0fb3d8c3527f8cff8805
Michael Gomes <michaelgomesweb@gmail.com>
2022-04-24 19:04:13 -0400

    Password Generation Functionality

--- src/screens/PasswordGeneratorScreen.js
+++ src/screens/PasswordGeneratorScreen.js
@@ -1,6 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PasswordGeneratorScreen = () => {
+  const [length, setLength] = useState(3);
+  const [symbols, setSymbols] = useState(false);
+  const [uppercase, setUppercase] = useState(false);
+  const [lowercase, setLowercase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [password, setPassword] = useState(
+    "Generated Password Will Appear Here"
+  );
+  var isSelected = false;
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    var charsLower = "abcdefghijklmnopqrstuvwxyz";
+    var charsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var charsNumbers = "0123456789";
+    var charsSpecial = '~!@#$%^&*()_+=[]{}";><?/';
+    var charsSelected = "";
+
+    if (symbols) {
+      isSelected = true;
+      charsSelected = charsSelected.concat(charsSpecial);
+    }
+    if (uppercase === true) {
+      isSelected = true;
+      charsSelected = charsSelected.concat(charsUpper);
+    }
+    if (lowercase === true) {
+      isSelected = true;
+      charsSelected = charsSelected.concat(charsLower);
+    }
+    if (numbers === true) {
+      isSelected = true;
+      charsSelected = charsSelected.concat(charsNumbers);
+    }
+    //If nothing is selected display message
+    if (isSelected === false) {
+      setPassword("Cannot leave selections blank");
+    }
+    //If atleast 1 option is selected generate password
+    if (isSelected) {
+      //Shuffles the entire possible chars list
+      charsSelected = charsSelected
+        .split("")
+        .sort(function () {
+          return 0.5 - Math.random();
+        })
+        .join("");
+
+      //Shortens it to the selected length
+      charsSelected = charsSelected.slice(0, length).toString();
+      setPassword(charsSelected);
+    }
+  }
+
+  function lengthChange(e) {
+    //Can't set value higher than 30
+    if (e.target.value <= 30) {
+      setLength(e.target.value);
+    }
+  }
+
+  function symbolsClick() {
+    if (symbols === false) {
+      setSymbols(true);
+    } else {
+      setSymbols(false);
+    }
+  }
+
+  function uppercaseClick() {
+    if (uppercase === false) {
+      setUppercase(true);
+    } else {
+      setUppercase(false);
+    }
+  }
+
+  function lowercaseClick() {
+    if (lowercase === false) {
+      setLowercase(true);
+    } else {
+      setLowercase(false);
+    }
+  }
+
+  function numbersClick() {
+    if (numbers === false) {
+      setNumbers(true);
+    } else {
+      setNumbers(false);
+    }
+  }
   return (
     <>
       <div className="generator-box">
@@ -12,7 +105,21 @@ const PasswordGeneratorScreen = () => {
                 <p>Password Length (3-30)</p>
               </td>
               <td>
-                <input type="number" min="3" max="30"></input>
+                <input
+                  onChange={lengthChange}
+                  type="number"
+                  min="3"
+                  max="30"
+                  value={length}
+                ></input>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>Include Uppercase ( ex. ABCDEFG )</p>
+              </td>
+              <td>
+                <input onClick={uppercaseClick} type="checkbox"></input>
               </td>
             </tr>
             <tr>
@@ -20,7 +127,7 @@ const PasswordGeneratorScreen = () => {
                 <p>Include Lowercase ( ex. abcdefg )</p>
               </td>
               <td>
-                <input type="checkbox"></input>
+                <input onClick={lowercaseClick} type="checkbox"></input>
               </td>
             </tr>
             <tr>
@@ -28,7 +135,7 @@ const PasswordGeneratorScreen = () => {
                 <p>Include Numbers ( ex. 1234567 )</p>
               </td>
               <td>
-                <input type="checkbox"></input>
+                <input onClick={numbersClick} type="checkbox"></input>
               </td>
             </tr>
             <tr>
@@ -36,16 +143,22 @@ const PasswordGeneratorScreen = () => {
                 <p>Include Symbols ( ex. ~!@#$% )</p>
               </td>
               <td>
-                <input type="checkbox"></input>
+                <input onClick={symbolsClick} type="checkbox"></input>
               </td>
             </tr>
-            <button className="btn">Generate</button>
+            <button className="btn" onClick={submitHandler}>
+              Generate
+            </button>
             <div>
               <p>
                 <u>Generated Password</u>
               </p>
               <p>
-                <input className="new-password" type="text"></input>
+                <input
+                  className="new-password"
+                  type="text"
+                  value={password}
+                ></input>
               </p>
               <p>
                 <button className="btn">Copy</button>
