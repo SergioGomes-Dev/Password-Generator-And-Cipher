2cd0ae2cd48f5caae248931b1d64b46aab9714ce
Michael Gomes <michaelgomesweb@gmail.com>
2022-04-24 19:18:01 -0400

    Home Screen Content

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <>
+      <div className="home">
+        <h1>Welcome!</h1>
+        <h2>
+          The entire color scheme of this website was designed with color
+          blindness in mind
+        </h2>
+        <h2>
+          Select one of the options at the top for the Password Generator or the
+          Cipher program
+        </h2>
+        <h2>We do not save any of the passwords/strings entered</h2>
+      </div>
+    </>
+  );
 };
 
 export default HomeScreen;
