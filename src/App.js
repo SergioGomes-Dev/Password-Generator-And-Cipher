6355c16240780c0bc7fb4facff804eefdfe270fa
Michael Gomes <michaelgomesweb@gmail.com>
2022-04-24 18:54:59 -0400

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  return <div>Password Generator & Cipher</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <button>Home</button>
+      <button>Password Generator</button>
+      <button>Cipher Program</button>
+    </header>
+  );
+};
+
+export default Header;
